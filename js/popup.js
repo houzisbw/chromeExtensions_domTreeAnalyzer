d8a79b5d9aead4a1dcb2f7b85421bf740c89b7ba
houzisbw <983134950@qq.com>
2018-06-13 22:47:40 +0800

    基本界面搭建完成

--- js/popup.js
+++ js/popup.js
@@ -2,12 +2,21 @@
  * 该js不注入页面，只是操作chrome插件弹出的面板，注意该js需要同注入页面的content-scripts进行通信
  * 该js无法获取dom以及操作网页js
  */
+//消息常量
+//开始分析
+var START_ANALYSIS = 'START_ANALYSIS';
 
 //获取开始按钮
-// var startBtn = document.getElementById('dom-tree-start-button');
-// startBtn.addEventListener('click',function(){
-//
-// });
+var startBtn = document.getElementById('dom-tree-start-button');
+startBtn.addEventListener('click',function(){
+	//展示loading界面
+	$('.dom-tree-start-btn-wrapper').hide();
+	$('.loading-analysis').show();
+	sendMessageToContentScript({cmd:START_ANALYSIS}, function(response) {
+		$('.loading-analysis').hide();
+		console.log(response);
+	});
+});
 
 
 
@@ -17,13 +26,8 @@
 function sendMessageToContentScript(message, callback) {
 	//查询当前激活的面板并发送数据,发送者为当前面板
 	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-		chrome.tabs.sendMessage(tabs[0].id, message, function(response)
-		{
+		chrome.tabs.sendMessage(tabs[0].id, message, function(response) {
 			if(callback) callback(response);
 		});
 	});
 }
-sendMessageToContentScript({cmd:'test', value:'你好，我是popup！'}, function(response)
-{
-	console.log('来自content的回复：'+response);
-});
