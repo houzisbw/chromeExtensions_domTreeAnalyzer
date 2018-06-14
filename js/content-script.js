16acacf801cfeb1c20540b7e217af1d2a0b892ef
houzisbw <983134950@qq.com>
2018-06-14 22:47:41 +0800

    后台js框架，css框架统计完成

--- js/content-script.js
+++ js/content-script.js
@@ -13,6 +13,8 @@
 			var ret = {};
 			//获取树的深度
 			ret.depthArguments = caculateDomTreeDepth(htmlNode);
+			//获取树各个节点的数量
+			ret.nodeArguments = analysisDomTree();
 			return ret
 		}
 	};
@@ -24,6 +26,7 @@
 		{
 			//收到消息并执行对应的处理方法
 			var returnValue = null;
+			console.log(request.cmd)
 			if(onMessageStrategy[request.cmd]!==undefined){
 				returnValue = onMessageStrategy[request.cmd]();
 				sendResponse(returnValue);
@@ -68,39 +71,13 @@
 		// 	});
 		//
 		// });
-		// //分析dom树
-		// function analysisDomTree(){
-		// 	//采用bfs遍历dom树
-		// 	var nodeQueue = [];
-		// 	nodeQueue.unshift(htmlNode);
-		// 	nodeWithMostChildren = htmlNode;
-		// 	while(nodeQueue.length){
-		// 		var curNode = nodeQueue.pop();
-		// 		//更新domNodeObj
-		// 		var nodeName = curNode.tagName.toLowerCase();
-		// 		if(domNodeObj[nodeName]===undefined){
-		// 			domNodeObj[nodeName]=1
-		// 		}else{
-		// 			domNodeObj[nodeName]++;
-		// 		}
-		// 		//更新儿子最多的节点
-		// 		var curChildrenNum = curNode.children.length;
-		// 		if(maxChildrenNum<curChildrenNum){
-		// 			maxChildrenNum = curChildrenNum;
-		// 			nodeWithMostChildren = curNode;
-		// 		}
-		// 		//将当前node的儿子节点加入queue中
-		// 		for(var i=0;i<curNode.children.length;i++){
-		// 			nodeQueue.unshift(curNode.children[i])
-		// 		}
-		// 	}
-		//
-		// }
+		//分析dom树
 
 
 	})
 
 	//分析方法
+
 	//获取dom树的深度
 	function caculateDomTreeDepth(root){
 		if(!root)return {depth:0,pathList:[]};
@@ -118,6 +95,108 @@
 			depth:tempDepth+1,
 			pathList:tempList
 		};
+	}
+	//获取dom树各个节点的数量,检测css框架使用的情况,检测使用框架的情况
+	function analysisDomTree(){
+		//js框架列表
+		var jsFrameworkObj = {};
+		//知名css框架名称
+		var wellKnownCssFrameworks = [
+			'iconfont','animated','bootstrap',
+			'bulma','semantic','foundation',
+			'uikit','miligram','kube','vital','pavilion',
+			'luxbar','waffle-grid','pure'];
+		var cssFrameworkList= [];
+		var domNodeObj = {};
+		var maxChildrenNum = 0;
+		//采用bfs遍历dom树
+		var nodeQueue = [];
+		nodeQueue.unshift(htmlNode);
+		var nodeWithMostChildren = htmlNode;
+		while(nodeQueue.length){
+			var curNode = nodeQueue.pop();
+			//更新domNodeObj
+			var nodeName = curNode.tagName.toLowerCase();
+			//如果是link节点
+			if(nodeName==='link'){
+				//获取该节点的rel属性
+				var rel = curNode.getAttribute('rel');
+				if(rel && rel === 'stylesheet'){
+					//获取href属性
+					var href = curNode.getAttribute('href');
+					if(href){
+						//获取最后的css
+						var splitted = href.split('/');
+						var cssName = splitted[splitted.length-1];
+						for(var i=0;i<wellKnownCssFrameworks.length;i++){
+							if(cssName.indexOf(wellKnownCssFrameworks[i])!==-1){
+								cssFrameworkList.push(wellKnownCssFrameworks[i])
+							}
+						}
+					}
+				}
+			}
+			//检测jquery
+			if(nodeName==='script'){
+				var src = curNode.getAttribute('src');
+				//必须判断src存在才能使用toLowerCase
+				if(src && src.toLowerCase().indexOf('jquery')!==-1){
+					jsFrameworkObj['jQuery']=1;
+				}
+			}
+			//检测三大框架
+			checkJsFramework(jsFrameworkObj,curNode);
+			//统计节点数目
+			if(domNodeObj[nodeName]===undefined){
+				domNodeObj[nodeName]=1
+			}else{
+				domNodeObj[nodeName]++;
+			}
+			//更新儿子最多的节点
+			var curChildrenNum = curNode.children.length;
+			if(maxChildrenNum<curChildrenNum){
+				maxChildrenNum = curChildrenNum;
+				nodeWithMostChildren = curNode;
+			}
+			//将当前node的儿子节点加入queue中
+			for(var i=0;i<curNode.children.length;i++){
+				nodeQueue.unshift(curNode.children[i])
+			}
+		}
+		//计算总结点个数
+		var nodeCnt = 0;
+		for(var key in domNodeObj){
+			if(domNodeObj.hasOwnProperty(key)){
+				nodeCnt+=domNodeObj[key]
+			}
+		}
+		return {
+			node:domNodeObj,
+			jsFrameworkList:jsFrameworkObj,
+			cssList:cssFrameworkList,
+			totalNodeNum:nodeCnt,
+			maxNodeNum:maxChildrenNum
+		};
+	}
+
+	//检测使用js框架的情况
+	function checkJsFramework(jsFrameworkObj,domNode){
+		//获取属性列表
+		var attrList = domNode.attributes;
+		var speicalAttr = {
+			'data-reactid':'React',
+			'data-v-':'Vue',
+			'ng-':'Angular'
+		};
+		//特征点数组
+		for(var i=0;i<attrList.length;i++){
+			var attrName = attrList[i].nodeName;
+			for(var key in speicalAttr){
+				if(attrName.indexOf(key)!==-1){
+					jsFrameworkObj[speicalAttr[key]]=1;
+				}
+			}
+		}
 	}
 
 })();
--- js/popup.js
+++ js/popup.js
@@ -2,20 +2,26 @@
  * 该js不注入页面，只是操作chrome插件弹出的面板，注意该js需要同注入页面的content-scripts进行通信
  * 该js无法获取dom以及操作网页js
  */
+
+
+
 //消息常量
 //开始分析
 var START_ANALYSIS = 'START_ANALYSIS';
 
-//获取开始按钮
+//获取开始分析按钮
 var startBtn = document.getElementById('dom-tree-start-button');
 startBtn.addEventListener('click',function(){
 	//展示loading界面
 	$('.dom-tree-start-btn-wrapper').hide();
 	$('.loading-analysis').show();
-	sendMessageToContentScript({cmd:START_ANALYSIS}, function(response) {
-		$('.loading-analysis').hide();
-		console.log(response);
-	});
+	//3秒延时是为了让网页尽量加载完全
+	setTimeout(function(){
+		sendMessageToContentScript({cmd:START_ANALYSIS}, function(response) {
+			$('.loading-analysis').hide();
+			console.log(response.nodeArguments.jsFrameworkList);
+		});
+	},3000);
 });
 
 
